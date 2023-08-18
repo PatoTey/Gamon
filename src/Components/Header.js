e61119f1d2fa51870d0cdbc78fffe1aa80aa3a2d
Teyo <73722247+PatoTey@users.noreply.github.com>
2023-08-18 19:15:23 -0400

    added better design, added icons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header(){
     return (
         <div className="header">
-            <h1>Gamon</h1>
+            <h1 className="sitetitle">Gamon</h1>
             <nav className="headernav">
                 <p className="navitem">Games</p>
                 <p className="navitem">About</p>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import Axios from "axios"
+import {FaRegHeart} from "react-icons/fa"
 
 export default function Home(){
     const [games, setGames] = useState([])
@@ -11,11 +12,16 @@ export default function Home(){
 
     const gamelist = games.map(game => {
         return (
-            <div className="game" key={game.id}>
-                <img className="gameimg" src={game.image} alt={game.name}/>
-                <div className="gamedata">
-                    <h2>{game.name}</h2>
-                    <h3> for ${game.price}</h3>
+            <div className="gamecard" key={game.id}>
+                <div className="gamelook">
+                    <img className="gameimg" src={game.image} alt={game.name}/>
+                    <div className="gamedata">
+                        <h2 className="gametitle">{game.name}</h2>
+                        <h3> for ${game.price} USD</h3>
+                    </div>
+                </div>
+                <div className="hearticon">
+                    <FaRegHeart/>
                 </div>
             </div>
         )
@@ -23,7 +29,13 @@ export default function Home(){
 
     return (
         <div className="home">
-            {gamelist}
+            <div className="gamelist">
+                {gamelist}
+                <p className="seeallbtn">See All</p>
+            </div>
+            <div className="boton all">
+
+            </div>
         </div>
     )
 }
